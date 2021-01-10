1c2740c02e63967ab0616dd2a9ff7daab8aaa213
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2021-01-10 14:53:37 +0200

    Solve R10 error

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("db ERROR: ", err.stack);
-    return;
-  }
-  console.log("db ID: ", db.threadId);
-});
+// db.connect((err) => {
+//   if (err) {
+//     console.error("db ERROR: ", err.stack);
+//     return;
+//   }
+//   console.log("db ID: ", db.threadId);
+// });
 
 module.exports = db;
