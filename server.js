f2bd825d60a867f55be883920f71774e46550ff7
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2021-01-11 00:50:49 +0200

    Convert axios url

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const path = require('path'); 
+const path = require('path');
 const socketio = require('socket.io');
 
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -37,8 +37,9 @@ app.use(cors());
 app.use(express.json());
 app.use(mainRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 || 80;
+const HOST = process.env.DB_HOST || '0.0.0.0';
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`listening at http://localhost:${PORT}`);
 });
